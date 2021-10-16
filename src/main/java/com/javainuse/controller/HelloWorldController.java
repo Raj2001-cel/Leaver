5d0cf94f56e85ddc1dcc53fa4ea949037669b89c
Raj Mourya <rajhmourya@gmail.com>
2021-10-16 10:53:18 +0530

    leaf is getting stored with the image in db only model prediction left

--- src/main/java/com/javainuse/controller/HelloWorldController.java
+++ src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.javainuse.controller;
 
 import com.javainuse.dao.ImageRepository;
+import com.javainuse.dao.LeafRepository;
 import com.javainuse.model.ImageModel;
+import com.javainuse.model.Leaf;
+import com.javainuse.service.LeafService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,23 +27,61 @@ public class HelloWorldController {
 	@Autowired
 	ImageRepository imageRepository;
 
-	@PostMapping("/upload")
-	public ResponseEntity<ImageModel> uplaodImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+
+	@Autowired
+	LeafService leafService;
+//	@PostMapping("/upload")
+//	public ResponseEntity<ImageModel> uplaodImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+//		System.out.println("Original Image Byte Size - " + file.getBytes().length);
+//		ImageModel img = new ImageModel(file.getOriginalFilename(), file.getContentType(),
+//				compressBytes(file.getBytes()));
+//		imageRepository.save(img);
+//		return ResponseEntity.status(HttpStatus.OK).body(img);
+//	}
+//
+//
+//
+//	@GetMapping(path = { "/upload/{imageName}" })
+//	public ImageModel getImage(@PathVariable("imageName") String imageName) throws IOException {
+//		final Optional<ImageModel> retrievedImage = imageRepository.findByName(imageName);
+//		ImageModel img = new ImageModel(retrievedImage.get().getName(), retrievedImage.get().getType(),
+//				decompressBytes(retrievedImage.get().getPicByte()));
+//		return img;
+//	}
+
+
+	@PostMapping(path = {"/predict/{reportId}"})
+	public ResponseEntity<Leaf> postImage(@RequestParam("imageFile") MultipartFile file,@PathVariable("reportId") Long reportId) throws IOException {
 		System.out.println("Original Image Byte Size - " + file.getBytes().length);
 		ImageModel img = new ImageModel(file.getOriginalFilename(), file.getContentType(),
 				compressBytes(file.getBytes()));
+
 		imageRepository.save(img);
-		return ResponseEntity.status(HttpStatus.OK).body(img);
+
+		boolean isUnhealthy = predict(img);
+		String Name = "pepper";
+		byte[] image  = img.getPicByte();
+		Leaf leaf = new Leaf();
+		leaf.setName(Name);
+		leaf.setImageBytes(image);
+		leaf.setImageName();
+		leaf.setUnhealthy(isUnhealthy);
+		leaf.setReportId(reportId);
+
+		leafService.saveLeaf(leaf);
+
+		return ResponseEntity.status(HttpStatus.OK).body(leaf);
 	}
 
 
+	public boolean predict(ImageModel imageModel){
+		//load the model
 
-	@GetMapping(path = { "/upload/{imageName}" })
-	public ImageModel getImage(@PathVariable("imageName") String imageName) throws IOException {
-		final Optional<ImageModel> retrievedImage = imageRepository.findByName(imageName);
-		ImageModel img = new ImageModel(retrievedImage.get().getName(), retrievedImage.get().getType(),
-				decompressBytes(retrievedImage.get().getPicByte()));
-		return img;
+		// prediction from the model
+
+		//return in boolean
+
+		return true;
 	}
 
 
